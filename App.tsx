9af9c84907442f68c96d15bb9daf433f3e1ca816
André Viapiana <andreviapiana@gmail.com>
2023-07-04 12:50:14 -0300

    ✨feat: Tema Global + Tipagem do Tema p/ Autocomplete

--- App.tsx
+++ App.tsx
@@ -2,15 +2,20 @@ import { StatusBar } from 'react-native'
 
 import { Home } from '@screens/Home'
 
+import { ThemeProvider } from 'styled-components'
+import theme from './src/theme'
+
 export default function App() {
   return (
-    <>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Home />
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Home />
+      </>
+    </ThemeProvider>
   )
 }
