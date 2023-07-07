878690bcce9b465fb7e465a587a16da0f2a6c5f4
André Viapiana <andreviapiana@gmail.com>
2023-07-07 10:01:07 -0300

    ✨feat: Página NewAndEdit - Inserção dos Inputs, FIlter e Button

--- src/screens/NewAndEdit/index.tsx
+++ src/screens/NewAndEdit/index.tsx
@@ -1,11 +1,93 @@
-import { Container } from './styles'
+import {
+  Container,
+  FilterTitle,
+  FilterWrapper,
+  Form,
+  IconWrapper,
+  InlineInputWrapper,
+  Title,
+} from './styles'
 
-import { Text } from 'react-native'
+import { ButtonIcon } from '@components/ButtonIcon'
+import { Input } from '@components/Input'
+import { Filter } from '@components/Filter'
+import { Button } from '@components/Button'
+
+import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
 export function NewAndEdit() {
+  // Navegando de volta para a página anterior //
+  const navigation = useNavigation()
+
+  function previousPage() {
+    navigation.goBack()
+  }
+
+  // Navegando para a página Feedback //
+  function handleFeedback() {
+    navigation.navigate('feedback')
+  }
+
+  // State inicial do Filter(o Sim começa selecionado) //
+  const [inDiet, setInDiet] = useState('Sim')
+
   return (
     <Container>
-      <Text>NewAndEdit</Text>
+      <IconWrapper>
+        <ButtonIcon icon="arrow-back" onPress={previousPage} />
+      </IconWrapper>
+
+      <Title>Nova Refeição</Title>
+
+      <Form>
+        <Input label="Nome" placeholder="Digite o nome" />
+
+        <Input
+          label="Descrição"
+          type="TEXTAREA"
+          multiline
+          numberOfLines={3}
+          placeholder="Digite uma descrição"
+        />
+
+        <InlineInputWrapper>
+          <Input
+            label="Data"
+            placeholder="Digite a data"
+            keyboardType="number-pad"
+            size={'SM'}
+          />
+
+          <Input
+            label="Hora"
+            placeholder="Digite a hora"
+            keyboardType="numeric"
+            size={'SM'}
+          />
+        </InlineInputWrapper>
+
+        <FilterWrapper>
+          <FilterTitle>Está dentro da dieta?</FilterTitle>
+          <FlatList
+            data={['Sim', 'Não']}
+            numColumns={2}
+            columnWrapperStyle={{ justifyContent: 'space-between' }}
+            keyExtractor={(item) => item}
+            renderItem={({ item }) => (
+              <Filter
+                style={{ width: '49%' }}
+                title={item}
+                isActive={item === inDiet}
+                onPress={() => setInDiet(item)}
+              />
+            )}
+          />
+        </FilterWrapper>
+
+        <Button title={'Cadastrar refeição'} onPress={handleFeedback} />
+      </Form>
     </Container>
   )
 }
--- src/screens/NewAndEdit/styles.ts
+++ src/screens/NewAndEdit/styles.ts
@@ -1,8 +1,55 @@
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_7};
-  padding: 48px 0 0;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_5};
+  padding: 32px 0 0;
+
+  align-items: center;
+  width: 100%;
+`
+
+export const IconWrapper = styled.View`
+  position: absolute;
+  top: 56px;
+  left: 24px;
+`
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_1};
+  font-size: ${({ theme }) => theme.FONT_SIZE.LG}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+`
+
+export const Form = styled.View`
+  flex: 1;
+  gap: 24px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+
+  margin-top: 34px;
+  padding: 33px 24px;
+`
+
+export const InlineInputWrapper = styled.View`
+  flex-direction: row;
+  gap: 12px;
+`
+
+export const FilterWrapper = styled.View`
+  width: 100%;
+  flex: 1;
+`
+
+export const FilterTitle = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_2};
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
+  margin-bottom: 4px;
+  align-self: flex-start;
 `
